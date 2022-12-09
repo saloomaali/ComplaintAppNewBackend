6df209449a9c3eb9e424431371c84ea1369e5f2b
Salooma <saloomaali2000@gmail.com>
2022-12-09 16:01:02 +0530

    api created

--- src/main/java/com/Nest/ComplaintBackend/Controller/ComplaintController.java
+++ src/main/java/com/Nest/ComplaintBackend/Controller/ComplaintController.java
@@ -1,10 +1,31 @@
 package com.Nest.ComplaintBackend.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class ComplaintController {
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/")
+    public String adminLogin(){
+        return "admin";
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/register")
+    public String userRegister(){
+        return "register";
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/userLogin")
+    public String userLogin(){
+        return "user";
+    }
 
 
 }
