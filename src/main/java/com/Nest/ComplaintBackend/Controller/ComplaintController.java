2eaedde6bfd6fb1ef124fab5715e68d11b6347e6
Salooma <saloomaali2000@gmail.com>
2022-12-09 15:31:23 +0530

    complaint backend

---
+++ src/main/java/com/Nest/ComplaintBackend/Controller/ComplaintController.java
@@ -0,0 +1,10 @@
+package com.Nest.ComplaintBackend.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ComplaintController {
+
+
+}
